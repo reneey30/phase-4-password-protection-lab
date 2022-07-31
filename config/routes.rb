893ab8238dec38184a97f12a0f5ab5453a1f9fbb
Irene Njeru <irenenjeru8@gmail.com>
2022-07-31 10:38:36 +0300

    TAsk completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # resources :users
+  get "/me", to: "users#show"
+  post "/signup", to: "users#create"
+  post "/login", to: "session#create"
+  delete "/logout", to: "session#destroy"
 end
